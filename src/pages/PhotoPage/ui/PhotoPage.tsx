5fa4c0a1476fbcac1ff48d9803e5bfa8b53c6b3a
Perevyazko <a.perevyazko@gmail.com>
2023-10-29 19:08:21 +0300

    setting view photo skeleton

--- src/pages/PhotoPage/ui/PhotoPage.tsx
+++ src/pages/PhotoPage/ui/PhotoPage.tsx
@@ -1,91 +1,91 @@
-import {memo, ReactNode, useState} from 'react';
+import React, {memo, ReactNode, useEffect, useState} from 'react';
 import {classNames, Mods} from "shared/lib/classNames/classNames";
 import {postApi} from "../../../providers/api/RtkService";
-import cls from "./PhotoPage.module.scss"
-import {useWindowWidth} from "shared/hooks/useWindowWidth/useWindowWidth";
+import cls from "./PhotoPage.module.scss";
 import Skeleton from 'react-loading-skeleton';
 import {PageWrapper} from "../../../shared/ui/PageWrapper/PageWrapper";
 
 interface PhotoPageProps {
-    className?: string
-    children?: ReactNode
+  className?: string,
+  children?: ReactNode
 }
-
 
 const PhotoPage = memo((props: PhotoPageProps) => {
 
-    const {data, isLoading, error} = postApi.useGetDataQuery({param:"",source:"api-photo/"})
-    const [isHovered, setIsHovered] = useState<number | null>(null);
-    const [isLoaded, setIsLoaded] = useState<number | null>(null);
-    const [loadedIndexes, setLoadedIndexes] = useState<number[]>([]);
-    const pageWidth = useWindowWidth();
+  const { data, isLoading, error } = postApi.useGetDataQuery({ param: "", source: "api-photo/" });
+  const [isHovered, setIsHovered] = useState<number | null>(null);
+  const [isLoaded, setIsLoaded] = useState<number | null>(null);
+  const [loadedIndexes, setLoadedIndexes] = useState<number[]>([]);
+  const [hoveredIndex, setHoveredIndex] = useState<number>(-1);
+  const [isAllLoaded, setIsAllLoaded] = useState(false); // Добавьте новое состояние isAllLoaded
 
-
-      const [hoveredIndex, setHoveredIndex] = useState<number>(-1);
-
-const handleRowHover = (index: number) => {
-  if ('ontouchstart' in window) {
-    if (index !== -1) {
-      setHoveredIndex(index);
+  const handleRowHover = (index: number) => {
+    if ('ontouchstart' in window) {
+      if (index !== -1) {
+        setHoveredIndex(index);
+      } else {
+        setHoveredIndex(-1);
+      }
     } else {
-      setHoveredIndex(-1);
+      setHoveredIndex(index);
     }
-  } else {
-    setHoveredIndex(index);
-  }
-};
+  };
 
-const handleRowLeave = () => {
-    // Обработка покидания курсора с элемента на десктопных устройствах
+  const handleRowLeave = () => {
     setHoveredIndex(-1);
-};
+  };
 
+  useEffect(() => {
+    if (data && loadedIndexes.length === data.length) {
+      setIsAllLoaded(true);
+    }
+  }, [data, loadedIndexes]);
 
-    const {
-        className,
-        children,
-        ...otherProps
-    } = props
+  const handleImageLoad = (index: number) => {
+    setLoadedIndexes(prevIndexes => [...prevIndexes, index]);
+  };
 
-    const mods: Mods = {
+  const {
+    className,
+    children,
+    ...otherProps
+  } = props;
 
-    };
+  const mods: Mods = {
 
-    return (
-        <PageWrapper>
+  };
+
+  return (
+    <PageWrapper>
+      <div
+        className={classNames(cls.PhotoPage, mods, [className])}
+        {...otherProps}
+      >
+        <div className={cls.WrapperBlockPhoto}>
+          {data && data.map((item, index) => (
             <div
-                className={classNames(cls.PhotoPage, mods, [className])}
-                {...otherProps}
+              className={cls.WrapperPhoto}
+              key={index}
             >
-                <div className={cls.WrapperBlockPhoto}>
-                                {data? data.map((item,index) => (
-                    <div
-                        className={cls.WrapperPhoto}
-                        key={index}
-                    >
-
-                        {/*{loadedIndexes.includes(index)?*/}
-
-
-                            <img
-                              className={(isHovered === index || hoveredIndex === index) ? cls.Photo : cls.PhotoHovered}
-                              onMouseEnter={() => handleRowHover(index)}
-                              onMouseLeave={handleRowLeave}
-                              onTouchStart={() => handleRowHover(index)}
-                              onLoad={() => setLoadedIndexes(prevIndexes => [...prevIndexes, index])}
-                              src={item.image}
-                            />
-                            {/*// : <Skeleton width={313} height={393} />}*/}
-
-
-                    </div>
-                )):<Skeleton width={313} height={393} />}
-                </div>
-
-                {children}
+              {(!isAllLoaded || !loadedIndexes.includes(index)) && <Skeleton className={cls.Photo}  />}
+                <img
+                  className={isLoaded ?
+                      (cls.PhotoNone):
+                      ((isHovered === index || hoveredIndex === index) ? cls.Photo : cls.PhotoHovered)
+                }
+                  onMouseEnter={() => handleRowHover(index)}
+                  onMouseLeave={handleRowLeave}
+                  onTouchStart={() => handleRowHover(index)}
+                  onLoad={() => handleImageLoad(index)} // Вызовите функцию обратного вызова при загрузке картинки
+                  src={item.image}
+                />
             </div>
-        </PageWrapper>
-    );
+          )) }
+        </div>
+        {children}
+      </div>
+    </PageWrapper>
+  );
 });
 
-export default PhotoPage
+export default PhotoPage;
