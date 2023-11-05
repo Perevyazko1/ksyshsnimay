63ca191c6f9fcaa50a3bd628f44bd02d43f4eee5
Perevyazko <a.perevyazko@gmail.com>
2023-11-05 11:18:53 +0300

    settings ICON site

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import "./app/style/index.scss"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './app/App';
-import {BrowserRouter, HashRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {setupStore} from "./app/store/store";
 import 'react-loading-skeleton/dist/skeleton.css'
